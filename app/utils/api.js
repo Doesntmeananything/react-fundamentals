ce527100e9d1d2a970d38e54dbf795a2b379dc5f
Doesntmeananything <andrey.krasnov.pm@gmail.com>
2019-05-24 20:29:32 +0300

    Add github api for profile comparison

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,6 +1,67 @@
 import axios from "axios";
+require("dotenv").config();
+
+const id = process.env.GITHUB_ID;
+const secret = process.env.GITHUB_SECRET;
+const params = `?client_id=${id}&client_secret=${secret}`;
+
+function getProfile(username) {
+  return axios
+    .get(`https://api.github.com/users${username}${params}`)
+    .then(function(user) {
+      return user.data;
+    });
+}
+
+function getRepos(username) {
+  return axios.get(
+    `https://api.github.com/users${username}/repos${params}$per_page=100`
+  );
+}
+
+function getStarCount(repos) {
+  return repos.data.reduce(function(count, repo) {
+    return count + repo.stargazers_count;
+  }, 0);
+}
+
+function calculateScore(profile, repos) {
+  const followers = profile.followers;
+  const totalStars = getStarCount(repos);
+
+  return followers * 3 + totalStars;
+}
+
+function handleError(error) {
+  console.warn(error);
+  return null;
+}
+
+function getUserData(player) {
+  return axios.all([getProfile(player), getRepos(player)]).then(function(data) {
+    const profile = data[0];
+    const repos = data[1];
+
+    return {
+      profile,
+      score: calculateScore(profile, repos)
+    };
+  });
+}
+
+function sortPlayers(players) {
+  return players.sort(function(a, b) {
+    return b.score - a.score;
+  });
+}
 
 export default {
+  battle: function(players) {
+    return axios
+      .all(players.map(getUserData))
+      .then(sortPlayers)
+      .catch(handleError);
+  },
   fetchPopularRepos: function(language) {
     let encodedURI = window.encodeURI(
       "https://api.github.com/search/repositories?q=stars:>1+language:" +
